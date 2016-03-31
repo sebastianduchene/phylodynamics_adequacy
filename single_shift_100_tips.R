223c3080115d1f35877eeca718d33940696d9734
Sebastian Duchene <sebastian.duchene@sydney.edu.au>
2016-03-31 16:05:28 +1100

    wow

--- single_shift_100_tips.R
+++ single_shift_100_tips.R
@@ -4,6 +4,32 @@ library(NELSI)
 library(doParallel)
 library(foreach)
 
+
+# Fit rate shifts
+fit_shifts<- function(tr, rho){
+    x_times <- sort(intnode.times(tr), decreasing = T)
+    start <- min(x_times)
+    end <- max(x_times)
+    grid <- diff(range(x_times))/50
+    res <- bd.shifts.optim(x_times, sampling = rho, grid, start, end, posdiv = T)
+    res[[2]]
+}
+
+# Pull out parameters for both numbers of shifts, shift time, and best model.
+get_params <- function(fit_model){
+    likelihoods <- sapply(fit_model, function(x) x[1])
+    test <- pchisq(2 * likelihoods[1] - likelihoods[2], 3)
+    turnovers <- c(fit_model[[1]][2], fit_model[[2]][c(2, 4)])
+    div_rates <-  c( fit_model[[1]][3], fit_model[[2]][c(3, 5)])
+    shift_time <- fit_model[[2]][6]
+    lambdas <- sapply(1:length(div_rates), function(x) div_rates[x] / (1-turnovers[x]))
+    mus <- lambdas * div_rates
+    best_n_shifts<- as.numeric(pchisq(2*(likelihoods[1] - likelihoods[2]), 3) > 0.95)
+    return(list(likelihoods = likelihoods, turnovers = turnovers, div_rates = div_rates, shift_time = shift_time, lambdas= lambdas, mus = mus, best_n_shifts = best_n_shifts))
+}
+
+
+
 set.seed(432134)
 nspecies <- 100
 time <- c(0, 0.5)
@@ -12,40 +38,17 @@ lambda <- c(1.5, 4)
 mu <- c(1.5, 0.1)
 rateshift_trees <- sim.rateshift.taxa(nspecies, 10, lambda = lambda, mu = mu, frac = rho, times = time, complete = F)
 
-# Fit rate shifts
-fit_shifts<- function(tr, rho){
-    x_times <- sort(intnode.times(tr), decreasing = T)
-    start <- min(x_times)
-    end <- max(x_times)
-    grid <- diff(range(x_times))/10
-    res <- bd.shifts.optim(x_times, sampling = rho, grid, start, end, posdiv = T)
-    res[[2]]
-}
 
-                                        # Pull out parameters for both numbers of shifts, shift time, and best model.
-#Debug HERE
-#get_params <- function(fit_model){
-    likelihoods <- sapply(fit_model, function(x) x[1])
-    test <- pchisq(2 * likelihoods[1] - likelihoods[2], 3)
-    turnovers <- c(fit_model[[1]][2], fit_model[[2]][c(2, 4)])
-    div_rates <-  c( fit_model[[1]][3], fit_model[[2]][c(3, 5)])
-    shift_time <- fit_model[[2]][6]
-    lambdas <- div_rates / (1-turnovers)
-    mus <- lambdas * div_rates
-  #  return(list(likelihoods, turnovers, div_rates, shift_time, lambdas, mus, test))
-#}
-
-
-lt1 <- fit_shifts(rateshift_trees[[1]], rho = rho)
+t1 <- fit_shifts(rateshift_trees[[1]], rho = rho)
+get_params(t1)
 mu/lambda
 lambda - mu
 
  # fit model,
-# pull out parameters estimated and save in a matrix,
-# use parameter estimates to simulate 100 trees under two models
+# pull out parameters estimated and save in a matrix with: likelihoods of the two models, model selected, simulated values, estimated values, parametric bootstrap likelihood hpd for one shift, parametric bootstrap likelihood hpd for no shifts.
 
 #loop_replicate <- function(tree, rho){
-
+# Include parallelisation here
 
 fit_model <-
 
