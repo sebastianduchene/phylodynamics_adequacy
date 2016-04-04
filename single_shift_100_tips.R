b9052820f9268b17890e7b2699e5bacc988b28c1
Sebastian Duchene <sebastian.duchene@sydney.edu.au>
2016-04-04 11:01:33 +1000

    updated one shift. Problems with lambda being lower than mu

--- single_shift_100_tips.R
+++ single_shift_100_tips.R
@@ -3,7 +3,9 @@ library(TreeSim)
 library(NELSI)
 library(doParallel)
 library(foreach)
-
+##
+# Simulate tree, fit parameters using one and two shifts, find best model using LRT,
+#get optimal parameters for one and two shifts and simulate 100 trees with each
 
 # Fit rate shifts
 fit_shifts<- function(tr, rho){
@@ -24,33 +26,46 @@ get_params <- function(fit_model){
     shift_time <- fit_model[[2]][6]
     lambdas <- sapply(1:length(div_rates), function(x) div_rates[x] / (1-turnovers[x]))
     mus <- lambdas * div_rates
+
+
+
     best_n_shifts<- as.numeric(pchisq(2*(likelihoods[1] - likelihoods[2]), 3) > 0.95)
     return(list(likelihoods = likelihoods, turnovers = turnovers, div_rates = div_rates, shift_time = shift_time, lambdas= lambdas, mus = mus, best_n_shifts = best_n_shifts))
 }
 
 
-
 set.seed(432134)
-nspecies <- 100
-time <- c(0, 0.5)
-rho <- c(1.0, 0.5) #all of the present day are sampled?
-lambda <- c(1.5, 4)
-mu <- c(1.5, 0.1)
+nspecies <- 50
+time <- c(0, 0.4)
+rho <- c(0.5, 1) #sampling proportion
+lambda <- c(2, 5)#spec rate
+mu <- c(1.5, 0.5)#extinction rate
+
+
 rateshift_trees <- sim.rateshift.taxa(nspecies, 10, lambda = lambda, mu = mu, frac = rho, times = time, complete = F)
 
+tr_fit <- fit_shifts(rateshift_trees[[1]], rho = rho)
 
-t1 <- fit_shifts(rateshift_trees[[1]], rho = rho)
-get_params(t1)
+tr_fit_params <- get_params(tr_fit)
+
+# Simulated parameters
 mu/lambda
 lambda - mu
+
+plot(rateshift_trees[[1]], show.tip.label = F)
+node_times <- intnode.times(rateshift_trees[[1]])
+nodelabels(round(node_times, 2), bg = 'white')
+lines(x = c(max(node_times)- tr_fit_params$shift_time, max(node_times) - tr_fit_params$shift_time), c(0, 100), col = rgb(1, 0, 0, 0.5), lwd = 6)
+
+sim_trees_one_shift <-sim.rateshift.taxa(nspecies, numbsim = 10, mu = tr_fit_params$mus[1], lambda = tr_fit_params$lambdas[1], frac = rho, times = tr_fit_params$shift_time, complete = F)
+
+
+
 
  # fit model,
 # pull out parameters estimated and save in a matrix with: likelihoods of the two models, model selected, simulated values, estimated values, parametric bootstrap likelihood hpd for one shift, parametric bootstrap likelihood hpd for no shifts.
 
 #loop_replicate <- function(tree, rho){
 # Include parallelisation here
-
-fit_model <-
-
 
 
